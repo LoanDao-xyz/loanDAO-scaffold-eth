9c49257c005a3702a7610d37dd021690410f8d82
Wary <one.should.be.wary@gmail.com>
2022-05-17 22:44:47 +0200

    🐛 vote state properly displayed

--- packages/react-app/src/views/MembershipVote.jsx
+++ packages/react-app/src/views/MembershipVote.jsx
@@ -1,31 +1,38 @@
+import { useEffect, useState } from "react";
 import { List } from "antd";
 import { useEventListener } from "eth-hooks/events/useEventListener";
 
 export default function MembershipVote({ contracts, contractName, eventName, localProvider, startBlock }) {
+  const [eventsWithState, setEventsWithState] = useState([]);
   // 📟 Listen for broadcast events
   const events = useEventListener(contracts, contractName, eventName, localProvider, startBlock);
   const tokenAddress = contracts && contracts.CommunityBankingToken ? contracts.CommunityBankingToken.address : null;
 
-  async function getProposalState(proposalId) {
-    const result = await contracts.CommunityBankingGovernor.state(proposalId);
-    console.log(result);
-    return result;
-  }
+  useEffect(async function() {
+    if (events?.length) {
+      const res = await Promise.all(events.map(async event => {
+        const state = await contracts.CommunityBankingGovernor.state(event.args[0]);
+        return { event, state };
+      }));
+
+      setEventsWithState(res)
+    }
+  }, [events, contracts]);
 
   return (
     <div style={{ width: "100%", margin: "auto", marginTop: 32, paddingBottom: 32 }}>
       <h2>Membership Proposals:</h2>
       <List
         bordered
-        dataSource={events}
-        renderItem={item => {
+        dataSource={eventsWithState}
+        renderItem={({ event, state }) => {
           return (
-            <List.Item key={item.args[0]}>
+            <List.Item key={event.args[0]}>
               <ul style={{ listStyleType: "none", textAlign: "left" }}>
-                <li>{`Prposal Id: ${item.args[0]}`}</li>
-                <li>{`Proposer: ${item.args[1]}`}</li>
-                {item.args[2] == tokenAddress && <li>{`Proposal Type: Membership`}</li>}
-                <li>{`Proposal State: ${getProposalState(item.args[0])}`}</li>
+                <li>{`Prposal Id: ${event.args[0]}`}</li>
+                <li>{`Proposer: ${event.args[1]}`}</li>
+                {event.args[2] == tokenAddress && <li>{`Proposal Type: Membership`}</li>}
+                <li>{`Proposal State: ${state}`}</li>
               </ul>
             </List.Item>
           );
